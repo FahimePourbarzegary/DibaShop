a5dc6991369f8bfa0a7e34672a6aca8b2a1808e5
fahime <fahimeh757@gmail.com>
2023-01-30 20:08:15 +0330

    Fix style

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
   return (
     <nav className=" flex justify-between items-center flex-wrap ">
       {/*Logo section */}
-      <div className=" font-vazirBold rounded-full  w-20 h-8 md:h-14 md:w-28 md:text-3xl bg-primary-300 text-dark flex justify-center items-center text-xl">
+      <div className=" font-vazirBold rounded-full  w-20 h-8   bg-primary-300 text-dark flex justify-center items-center text-xl">
         دیبا
       </div>
       <div className="transition-all duration-300 ">
@@ -34,15 +34,15 @@ export const Navbar = () => {
             : " overflow-hidden max-h-0 opacity-0"
         }  md:max-h-screen md:opacity-100 rounded md:w-auto  duration-500 ease-in-out `}
       >
-        <ul className="p-2 text-sm flex flex-col md:flex-row duration-500 md:text-lg">
-          <li className="p-1 py-2 mb-2 md:mx-2  md:mb-0 hover:bg-primary-200 hover:text-dark rounded-lg md:px-4 md:py-2  transition-all font-semibold md:font-medium">
+        <ul className="p-2 text-sm flex flex-col md:flex-row duration-500">
+          <li className="p-1 py-2 mb-2 md:mx-2  md:mb-0 hover:bg-primary-200 hover:text-dark rounded-lg md:px-4 md:py-2  transition-all text-sm">
             <NavLink to="/" className="w-full block ">
               صفحه اصلی
             </NavLink>
           </li>
           {/*Drop Down section */}
           <li
-            className="p-1 py-2 mb-2 md:mx-2 md:mb-0 hover:bg-primary-200 hover:text-dark rounded-lg md:px-4 md:py-2   transition-all font-semibold md:font-medium"
+            className="p-1 py-2 mb-2 md:mx-2 md:mb-0 hover:bg-primary-200 hover:text-dark rounded-lg md:px-4 md:py-2   transition-all text-sm"
             onMouseEnter={() => {
               setDropdownCate("max-h-screen opacity-100");
             }}
@@ -50,13 +50,13 @@ export const Navbar = () => {
               setDropdownCate("overflow-hidden max-h-0 opacity-0")
             }
           >
-            <button className="flex items-center justify-between w-full   rounded md:hover:bg-transparent md:border-0  md:p-0 md:w-auto  font-semibold md:font-medium hover:text-dark">
+            <button className="flex items-center justify-between w-full   rounded md:hover:bg-transparent md:border-0  md:p-0 md:w-auto   hover:text-dark text-sm">
               دسته بندی
             </button>
             <div
               className={`z-10  font-normal bg-dark divide-y divide-gray-100 rounded shadow w-44 absolute top-30 md:top-16 ${dropdownCate}  duration-500`}
             >
-              <ul className="p-1 py-2 md:font-medium text-primary-200">
+              <ul className="p-1 py-2  text-primary-200">
                 <li className="block px-4 py-2 rounded  hover:bg-primary-200 hover:text-dark ">
                   <NavLink to="/" className="w-full block">
                     گردنبند
@@ -75,17 +75,17 @@ export const Navbar = () => {
               </ul>
             </div>
           </li>
-          <li className="p-1 py-2 mb-2 md:mx-2 md:mb-0 hover:bg-primary-200 hover:text-dark rounded-lg md:px-4 md:py-2  transition-all font-semibold md:font-medium">
+          <li className="p-1 py-2 mb-2 md:mx-2 md:mb-0 hover:bg-primary-200 hover:text-dark rounded-lg md:px-4 md:py-2  transition-all  text-sm">
             <NavLink to="/" className="w-full block ">
               وبلاگ
             </NavLink>
           </li>
-          <li className="p-1 py-2 mb-2 md:mx-2 md:mb-0 hover:bg-primary-200 hover:text-dark rounded-lg md:px-4 md:py-2  transition-all font-semibold md:font-medium">
+          <li className="p-1 py-2 mb-2 md:mx-2 md:mb-0 hover:bg-primary-200 hover:text-dark rounded-lg md:px-4 md:py-2  transition-all text-sm ">
             <NavLink to="/" className="w-full block ">
               درباره ما
             </NavLink>
           </li>
-          <li className="p-1 py-2 mb-2 md:mx-2 md:mb-0 bg-primary-200 text-dark rounded-lg md:px-4 md:py-2  transition-all font-semibold">
+          <li className="p-1 py-2  md:mx-2 md:mb-0 bg-primary-200 text-dark rounded-lg md:px-4 md:py-2  transition-all font-semibold text-sm">
             <NavLink
               to="/"
               className="w-full  
