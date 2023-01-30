cc055cb92e7856f8a4a64bf3cb0cca2976d0cf09
fahime <fahimeh757@gmail.com>
2023-01-30 13:56:54 +0330

    add style and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import {Routes,Route} from "react-router-dom"
-import HomePage from "./Pages/HomePage"
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>}/>
+      <Route path="/" element={<HomePage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      bg: {
-        dark: "#1A202C",
+      ...colors,
+      dark: "#1A202C",
+      white: "#FFFFFF",
+      transparent: "transparent",
+      current: "currentColor",
+      primary: {
+        100: "#FFE488",
+        200: "#FFD96B",
+        300: "#FDE047",
+        400: "#F59E0B",
+      },
+      gray: {
+        100: "#C3D4E9",
+        200: "#Car",
+        300: "#596780",
+      },
+      warning: "#FF0F0F",
+    },
+    extend: {
+      fontFamily: {
+        vazirBold: ["vazir-bold", "sans-serif"],
+        vazirLight: ["vazir-light", "sans-serif"],
+        vazirMedium: ["vazir-medium", "sans-serif"],
+        vazirThin: ["vazir-thin", "sans-serif"],
+        vazirRegular: ["vazir-regular", "sans-serif"],
       },
     },
-    extend: {},
   },
+
   plugins: [],
 };
