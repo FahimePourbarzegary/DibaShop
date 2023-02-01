5a31c4c7993be8495fed1525ed1a8e3d05016550
fahime <fahimeh757@gmail.com>
2023-02-01 09:33:55 +0330

    add layout

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import Carousel from "../Components/Carousel";
+import Footer from "../Components/Footer";
+import { Navbar } from "../Components/Navbar";
+type LayoutProps = {
+  children: ReactNode;
+};
+function Layout({ children }: LayoutProps) {
+  return (
+    <div>
+      <header className="flex flex-col py-4 px-6 md:px-8 ">
+        <Navbar />
+      </header>
+      <Carousel />
+      <main className=" px-4">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
