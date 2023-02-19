da66352a7eb0babfe3bc4f3861ecd5ed879a67ac
fahime <fahimeh757@gmail.com>
2023-02-19 18:41:52 +0330

    load user data in userDashboard

--- src/Pages/UserInfoPage.tsx
+++ src/Pages/UserInfoPage.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { useAppSelector } from "../hooks/hooks";
+import { RootState } from "../services/store";
 
 function UserInfoPage() {
+  const { user, loggedIn, error } = useAppSelector(
+    (state: RootState) => state.users
+  );
   return (
     <section className="py-4 w-full">
       <p className=" pr-8  md:pr-18 font-semibold text-slate-600 text-base md:text-xl">
@@ -11,17 +15,17 @@ function UserInfoPage() {
         md:grid-cols-2 md:gap-8 xl:grid-cols-3"
       >
         <div className="mt-3 p-3 rounded-xl bg-slate-800 text-slate-400  focus:outline-none focus:ring focus:ring-primary-200">
-          نام{" "}
+          {user?.name}
         </div>
         <div className="mt-3 p-3 rounded-xl bg-slate-800 text-slate-400  focus:outline-none focus:ring focus:ring-primary-200">
-          نام خانوادگی
+          {user?.lastname}
         </div>
 
         <div className="mt-3 p-3 rounded-xl bg-slate-800 text-slate-400  focus:outline-none focus:ring focus:ring-primary-200">
-          ایمیل
+          {user?.email}
         </div>
         <div className="p-3 mt-3 rounded-xl bg-slate-800 text-slate-400 overflow-x-scroll   whitespace-nowrap">
-          آدرس،آدرس،آدرس،آدرس آدرس،آدرس،آدرس،آدرس آدرس،آدرس،آدرس،آدرس
+          {user?.address}
         </div>
       </div>
     </section>
