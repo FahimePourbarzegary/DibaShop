dda4a3ff9b83f803fa37c4b39187f6555bf1804b
fahime <fahimeh757@gmail.com>
2023-02-19 18:40:51 +0330

    operation Register user

--- src/Pages/RegisterPage.tsx
+++ src/Pages/RegisterPage.tsx
@@ -1,11 +1,14 @@
 import { useFormik } from "formik";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import * as yup from "yup";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import { autoLogin, RegisterUser } from "../services/features/userSlice";
+import { RootState } from "../services/store";
 type initialValuesType = {
   name: string;
-  lastName: string;
-  userName: string;
+  lastname: string;
+  username: string;
   email: string;
   address: string;
   password: string;
@@ -15,8 +18,8 @@ function RegisterPage() {
   //initial value formik
   let initialValues: initialValuesType = {
     name: "",
-    lastName: "",
-    userName: "",
+    lastname: "",
+    username: "",
     email: "",
     address: "",
     password: "",
@@ -25,7 +28,7 @@ function RegisterPage() {
 
   //for validation inputs
   const validationSchema = yup.object({
-    userName: yup
+    username: yup
       .string()
       .max(15, " نام کاربری باید حداکثر 15 و حداقل 3 کارکتر باشد.")
       .min(3, " نام کاربری باید حداکثر 15 و حداقل 3 کارکتر باشد.")
@@ -35,7 +38,7 @@ function RegisterPage() {
       .max(15, " نام باید حداکثر 15 و حداقل 3 کارکتر باشد.")
       .min(3, " نام باید حداکثر 15 و حداقل 3 کارکتر باشد.")
       .required("نام را وارد کنید"),
-    lastName: yup
+    lastname: yup
       .string()
       .max(20, "نام خانوادگی  باید حداکثر 20 و حداقل 3 کارکتر باشد.")
       .min(3, "نام خانوادگی  باید حداکثر 20 و حداقل 3 کارکتر باشد.")
@@ -56,9 +59,37 @@ function RegisterPage() {
       .required("تکرار رمز را وارد کنید"),
   });
   // send values and register
-  const onSubmit = (values: initialValuesType) => {
-    console.log(values);
-    console.log(formik.values);
+  const navigate = useNavigate();
+  const { user, loading, loggedIn, error } = useAppSelector(
+    (state: RootState) => state.users
+  );
+  const dispatch = useAppDispatch();
+  const [errorUser, setErrorUser] = useState<null | string>(null);
+  useEffect(() => {
+    if (loggedIn) {
+      setErrorUser(null);
+      dispatch(autoLogin());
+      navigate("/");
+    }
+  }, [loggedIn]);
+  const onSubmit = async (values: initialValuesType) => {
+    const { name, lastname, username, email, password, address } = values;
+    console.log(loggedIn);
+
+    await dispatch(
+      RegisterUser({
+        id: new Date().getTime(),
+        name,
+        lastname,
+        username,
+        email,
+        password,
+        address,
+        avatar: "",
+      })
+    );
+
+    if (!loggedIn) setErrorUser("نام کاربری یا ایمیل از قبل وجود دارد");
   };
   //*******************/
   const formik = useFormik({
@@ -74,6 +105,32 @@ function RegisterPage() {
         onSubmit={formik.handleSubmit}
       >
         {/*Error box */}
+        {loading ? (
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="w-8 h-8 mr-2 text-gray-200 animate-spin  fill-primary-400"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : null}
+        {errorUser ? (
+          <div className=" bg-warning w-full text-sm rounded-lg p-2">
+            {errorUser}
+          </div>
+        ) : null}
         <div
           className={` bg-warning w-full text-sm rounded-lg  ${
             Object.keys(formik.errors).length ? "block" : "hidden"
@@ -81,8 +138,8 @@ function RegisterPage() {
         >
           <ul>
             <li>
-              {formik.touched.userName && formik.errors.userName && (
-                <div className="p-2">{formik.errors.userName}</div>
+              {formik.touched.username && formik.errors.username && (
+                <div className="p-2">{formik.errors.username}</div>
               )}
             </li>
             <li>
@@ -91,8 +148,8 @@ function RegisterPage() {
               )}
             </li>
             <li>
-              {formik.touched.lastName && formik.errors.lastName && (
-                <div className="p-2">{formik.errors.lastName}</div>
+              {formik.touched.lastname && formik.errors.lastname && (
+                <div className="p-2">{formik.errors.lastname}</div>
               )}
             </li>
             <li>
@@ -123,11 +180,11 @@ function RegisterPage() {
         <div className="grid  grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col">
             {" "}
-            <label htmlFor="userName">نام کاربری :</label>
+            <label htmlFor="username">نام کاربری :</label>
             <input
               type="text"
-              id="userName"
-              {...formik.getFieldProps("userName")}
+              id="username"
+              {...formik.getFieldProps("username")}
               className="mt-3 p-3 rounded-xl bg-slate-800 text-slate-400  focus:outline-none focus:ring focus:ring-primary-200"
               placeholder="نام کاربری"
             />
@@ -145,11 +202,11 @@ function RegisterPage() {
           </div>
           <div className="flex flex-col">
             {" "}
-            <label htmlFor="lastName">نام خانوادگی :</label>
+            <label htmlFor="lastname">نام خانوادگی :</label>
             <input
               type="text"
-              {...formik.getFieldProps("lastName")}
-              id="lastName"
+              {...formik.getFieldProps("lastname")}
+              id="lastname"
               className="mt-3 p-3 rounded-xl bg-slate-800 text-slate-400  focus:outline-none focus:ring focus:ring-primary-200"
               placeholder="نام خانوادگی"
             />
@@ -182,7 +239,7 @@ function RegisterPage() {
               type="password"
               {...formik.getFieldProps("password")}
               id="password"
-              className="mt-3 p-3 rounded-xl bg-slate-800 password-slate-400  focus:outline-none focus:ring focus:ring-primary-200"
+              className="mt-3 p-3 rounded-xl bg-slate-800 password-slate-400 text-slate-400  focus:outline-none focus:ring focus:ring-primary-200"
               placeholder="رمز"
             />
           </div>
@@ -190,7 +247,7 @@ function RegisterPage() {
             {" "}
             <label htmlFor="confirmPassword">تکرار رمز :</label>
             <input
-              type="text"
+              type="password"
               {...formik.getFieldProps("confirmPassword")}
               id="confirmPassword"
               className="mt-3 p-3 rounded-xl bg-slate-800 text-slate-400  focus:outline-none focus:ring focus:ring-primary-200"
