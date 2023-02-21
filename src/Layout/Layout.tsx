c457482f91b57b77b4b604076a0efb376dbccce2
fahime <fahimeh757@gmail.com>
2023-02-21 16:32:23 +0330

    add toast library

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import Footer from "../Components/Footer";
 import { Navbar } from "../Components/Navbar";
 import { useAppDispatch } from "../hooks/hooks";
 import { autoLogin } from "../services/features/userSlice";
+import { Toaster } from "react-hot-toast";
 type LayoutProps = {
   children: ReactNode;
 };
@@ -13,6 +14,22 @@ function Layout({ children }: LayoutProps) {
   }, []);
   return (
     <div>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: "",
+          duration: 5000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+        }}
+      />
       <header className="flex flex-col py-4 px-6 md:px-8 ">
         <Navbar />
       </header>
